58ce5ca254721bb4ac187b9d246ac5628b9877f9
Jonathan King <jking@fearless.tech>
2018-07-26 13:13:22 +0000

    TA-490 adds a circleci build

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker run -e "BASE_URL=https://content.${1}" \
 -e "S3BUCKET=sbagovlower-test-results" \
--e "TARGET_ENV=${1}"  -e "TEST_USER=${2}"  -e "TEST_PASSWORD=${3}" -v ~/.aws:/home/chrome/.aws sba-gov-cypress
+-e "TARGET_ENV=${1}"  -e "TEST_USER=${2}"  -e "TEST_PASSWORD=${3}" -v ~/.aws:/home/chrome/.aws sba-gov-integration-tests
