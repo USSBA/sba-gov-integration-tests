70003a5936b55c00261d44e1eba875928918beb6
Jonathan King <jking@fearless.tech>
2018-07-26 18:59:58 +0000

    TA-490 adds a workaround for the drupal http issue

--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 docker run -e "BASE_URL=https://content.${1}" \
--e "S3BUCKET=sbagovlower-test-results" \
 -e "TARGET_ENV=${1}"  -e "TEST_USER=${2}"  -e "TEST_PASSWORD=${3}" -v ~/.aws:/home/chrome/.aws sba-gov-integration-tests
