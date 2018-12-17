f64a1126914d7f8b2da1477619e2dfd1ec677803
Alex de los Reyes <cyberdecker@gmail.com>
2018-12-17 16:09:06 -0500

    TA-492 Adding drupal 8 test suite with modifications for local  testing.

---
+++ run-local.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+export CYPRESS_BASE_URL="https://content.${1}.ussba.io" 
+export CYPRESS_TARGET_ENV="${1}" 
+export CYPRESS_TEST_USER="${2}" 
+export CYPRESS_TEST_PASSWORD="${3}" 
+./node_modules/.bin/cypress open --config chromeWebSecurity=false
