dc5c30e08f8fe87aa96169bb655e357d47ae79a7
Alex de los Reyes <cyberdecker@gmail.com>
2019-01-08 12:31:38 -0500

    TA-1384 Reformatting tests and adding tests for twitter metadata

--- cypress/integration/tests/basic-page.test.js
+++ cypress/integration/tests/basic-page.test.js
@@ -1,18 +1,32 @@
 describe('Basic page', function() {
-  it('Should have head metadata', function() {
+  it('head section should have Open Graph metadata', function() {
     cy.visit('/business-guide/plan-your-business/write-your-business-plan')
 
-    // Checks title metadata
+    // Checks open graph title metadata
     cy.get('#titleSectionTitleId').invoke('text').then((title) => {
       cy.get('head meta[property="og:title"]').should('have.attr', 'content', title)
-
+      cy.get('head meta[property="og:site_name"]').should('have.attr', 'content', title)
       // This is the alternative to should, using expect instead which requires using a jquery element
       // expect(Cypress.$('head meta[property="og:title"]')).to.have.attr("content", title)
     })
 
-    // Checks description metadata
+    // Checks open graph description metadata
     cy.get('#titleSectionSummaryId').invoke('text').then((description) => {
       cy.get('head meta[property="og:description"]').should('have.attr', 'content', description)
+    })
+  })
+
+  it('head section should have Twitter metadata', function() {
+    cy.visit('/business-guide/manage-your-business/manage-your-finances')
+
+    // Checks twitter title metadata
+    cy.get('#titleSectionTitleId').invoke('text').then((title) => {
+      cy.get('head meta[name="twitter:title"]').should('have.attr', 'content', title)
+    })
+
+    // Checks twitter description metadata
+    cy.get('#titleSectionSummaryId').invoke('text').then((description) => {
+      cy.get('head meta[name="twitter:description"]').should('have.attr', 'content', description)
     })
   })
 })
