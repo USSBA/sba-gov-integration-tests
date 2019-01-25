4b9b59408dd75005746459b76e3782c7d9a8b034
Aaron Brooks <brooksa321@gmail.com>
2019-01-25 11:48:05 -0500

    removed apostraphe from events

--- cypress/integration/tests/events-find.test.js
+++ cypress/integration/tests/events-find.test.js
@@ -1,7 +1,7 @@
 describe("Events Find Page", function () {
     it('should load and have a title', function () {
         cy.visit("/events/find")
-        cy.get("h3").should("have.text", "event's find")
+        cy.get("h3").should("have.text", "events find")
         cy.get("head > title").should("have.text", "Small Business Administration")
     })
 })
