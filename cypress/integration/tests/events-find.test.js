223fc5cd6a99beb75795b256f0afc01c3c288ed1
Aaron Brooks <brooksa321@gmail.com>
2019-01-28 10:23:32 -0500

    changed event to events

--- cypress/integration/tests/events-find.test.js
+++ cypress/integration/tests/events-find.test.js
@@ -1,6 +1,6 @@
 describe("Events Find Page", function () {
     it('should load and have a title', function () {
-        cy.visit("/event/find")
+        cy.visit("/events/find")
         cy.get("h3").should("have.text", "events find")
         cy.get("head > title").should("have.text", "Small Business Administration")
     })
