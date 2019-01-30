cb785f68ee03c030b8baea75142ef6e6680b5890
Alex de los Reyes <cyberdecker@gmail.com>
2019-01-30 12:04:26 -0500

    TA-1568 Rebase to include latest code

--- cypress/integration/tests/events-find.test.js
+++ cypress/integration/tests/events-find.test.js
@@ -1,6 +1,7 @@
 describe("Events Find Page", function () {
     it('should load and have a title', function () {
         cy.visit("/events/find")
+<<<<<<< HEAD
         cy.get("head > title").should("have.text", "Small Business Administration")
     })
 
@@ -20,5 +21,42 @@ describe("Events Find Page", function () {
         cy.get("@SubmitButton").click()
         cy.get("@EventKeyword").invoke("val").should("equal", expectedKeyword)
         cy.url().should("include", `q=${expectedKeyword}`)
+=======
+        // TODO: replace when changed from office
+        cy.get('#office-primary-search-bar-title').should("have.text", "Find events")
+        cy.get("head > title").should("have.text", "Small Business Administration")
+    })
+
+    it('should have a submit button', function() {
+        cy.visit("/events/find")
+        // TODO: replace when changed from office
+        cy.get('#office-primary-search-bar-search-button').should('exist')
+    })
+
+    it('should have a labeled zip code field', function(){
+        cy.visit("/events/find")
+        cy.get("[data-cy='zip']").as("ZipInput")
+        cy.get("@ZipInput").should("exist")
+        cy.get("@ZipInput").parent().siblings("label").as("ZipLabel")
+        cy.get("@ZipLabel").should("have.text", "Near")
+    })
+
+    it('should allow valid zip codes to be entered', function(){
+        cy.visit("/events/find")
+        cy.get("[data-cy='zip']").as("ZipInput")
+        cy.get("@ZipInput").type("99999")
+        // TODO: replace when changed from office
+        cy.get('#office-primary-search-bar-search-button').click();
+        cy.get("@ZipInput").invoke("val").should("eq","99999")
+    })
+
+    it('should display an error for non-numeric zip codes', function(){
+        cy.visit("/events/find")
+        cy.get("[data-cy='zip']").as("ZipInput")
+        cy.get("@ZipInput").type("abcde")
+        // TODO: replace when changed from office
+        cy.get('#office-primary-search-bar-search-button').click();
+        cy.get('#zip-error').should("exist")
+>>>>>>> 24c9310... TA-1568 Filling out tests for zip code search
     })
 })
