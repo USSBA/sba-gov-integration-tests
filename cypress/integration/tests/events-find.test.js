a94b549bf9d133d1e6bd13348c5c524fbb98a844
Alex de los Reyes <cyberdecker@gmail.com>
2019-02-04 09:35:51 -0500

    TA-1566 adding keyword field tests

--- cypress/integration/tests/events-find.test.js
+++ cypress/integration/tests/events-find.test.js
@@ -1,7 +1,27 @@
 describe("Events Find Page", function () {
     it('should load and have a title', function () {
         cy.visit("/events/find")
-        cy.get("h3").should("have.text", "events find")
         cy.get("head > title").should("have.text", "Small Business Administration")
     })
+
+    it("should have a labeled keyword field", function(){
+        cy.visit("/events/find")
+        cy.get("#event-lookup-text-input").as("EventKeyword")
+        cy.get("@EventKeyword").should("exist")
+        cy.get("@EventKeyword").parent().siblings().get('label').should("have.text", "Search")
+    })
+
+    it("takes a string of text as a keyword", function(){
+        const expectedKeyword = "testkeyword"
+        cy.visit("/events/find")
+        cy.get("#event-lookup-text-input").as("EventKeyword")
+        cy.get("button.button").as("SubmitButton")
+        cy.get("@EventKeyword").type(expectedKeyword)
+        cy.get("@SubmitButton").click()
+        cy.get("@EventKeyword").invoke("val").should("equal", expectedKeyword)
+        cy.url().should("include", `keyword=${expectedKeyword}`)
+    })
+
+
+
 })
