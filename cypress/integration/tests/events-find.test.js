342dae43d15b38aa5942288654af878f106e4a87
Alex de los Reyes <cyberdecker@gmail.com>
2019-02-05 14:58:55 -0500

    TA-1566 updating event tests to include correct identifiers for keyword

--- cypress/integration/tests/events-find.test.js
+++ cypress/integration/tests/events-find.test.js
@@ -6,7 +6,7 @@ describe("Events Find Page", function () {
 
     it("should have a labeled keyword field", function(){
         cy.visit("/events/find")
-        cy.get("#event-lookup-text-input").as("EventKeyword")
+        cy.get("[data-cy='keyword search']").as("EventKeyword")
         cy.get("@EventKeyword").should("exist")
         cy.get("@EventKeyword").parent().siblings().get('label').should("have.text", "Search")
     })
@@ -14,14 +14,11 @@ describe("Events Find Page", function () {
     it("takes a string of text as a keyword", function(){
         const expectedKeyword = "testkeyword"
         cy.visit("/events/find")
-        cy.get("#event-lookup-text-input").as("EventKeyword")
+        cy.get("[data-cy='keyword search']").as("EventKeyword")
         cy.get("button.button").as("SubmitButton")
         cy.get("@EventKeyword").type(expectedKeyword)
         cy.get("@SubmitButton").click()
         cy.get("@EventKeyword").invoke("val").should("equal", expectedKeyword)
-        cy.url().should("include", `keyword=${expectedKeyword}`)
+        cy.url().should("include", `q=${expectedKeyword}`)
     })
-
-
-
 })
