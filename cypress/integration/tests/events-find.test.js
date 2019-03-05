6f7124a201a676738f928cfd7ee7666b260fd9f2
Alex de los Reyes <cyberdecker@gmail.com>
2019-03-05 17:04:12 -0500

    TA-1569 Modifying tests for exist and adding tests for events results

--- cypress/integration/tests/events-find.test.js
+++ cypress/integration/tests/events-find.test.js
@@ -7,7 +7,7 @@ describe("Events Find Page", function () {
     it("has a labeled keyword field", function(){
         cy.visit("/events/find")
         cy.get("[data-cy='keyword search']").as("EventKeyword")
-        cy.get("@EventKeyword").should("exist")
+        cy.get("@EventKeyword")
         cy.get('label[for="keyword-search"]').should("have.text", "Search")
     })
 
@@ -22,12 +22,12 @@ describe("Events Find Page", function () {
 
     it("has a submit button", function() {
         cy.visit("/events/find")
-        cy.get('[data-cy="search button"]').should('exist')
+        cy.get('[data-cy="search button"]')
     })
 
     it("has a labeled zip code field", function(){
         cy.visit("/events/find")
-        cy.get("[data-cy='zip']").as("ZipInput").should("exist")
+        cy.get("[data-cy='zip']").as("ZipInput")
         cy.get('label[for="zip"]').should("have.text", "Near")
     })
 
@@ -44,7 +44,7 @@ describe("Events Find Page", function () {
         cy.get("[data-cy='zip']").as("ZipInput")
         cy.get("@ZipInput").type("abcde")
         cy.get('#events-primary-search-bar-search-button').click();
-        cy.get('#zip-error').should("exist")
+        cy.get('#zip-error')
     })
 
     it("sets query parameters from form values", function(){
@@ -85,11 +85,12 @@ describe("Events Find Page", function () {
         cy.get('label[for="date-filter"]').should("have.text", "Date Range")
         cy.get("[data-cy='date']").as("DateRange")
         cy.get("@DateRange").click().find(".Select-menu-outer").as("DateRangeOptions")
-        cy.get("@DateRangeOptions").contains("All Upcoming").should("exist")
-        cy.get("@DateRangeOptions").contains("Today").should("exist")
-        cy.get("@DateRangeOptions").contains("Tomorrow").should("exist")
-        cy.get("@DateRangeOptions").contains("Next 7 Days").should("exist")
-        cy.get("@DateRangeOptions").contains("Next 30 Days").should("exist")
+
+        cy.get("@DateRangeOptions").contains("All Upcoming")
+        cy.get("@DateRangeOptions").contains("Today")
+        cy.get("@DateRangeOptions").contains("Tomorrow")
+        cy.get("@DateRangeOptions").contains("Next 7 Days")
+        cy.get("@DateRangeOptions").contains("Next 30 Days")
     })
 
     it("has a date range default of All Upcoming", function() {
@@ -104,28 +105,16 @@ describe("Events Find Page", function () {
 
     it('submits an events.json request when a search is submitted', function(){
         cy.server()
-        cy.route("GET", "/api/content/events.json**").as("EventsRequest")
+        cy.route("GET", "/api/content/events.json?pageSize=10&start=0&dateRange=tomorrow&distance=50&q=test&address=99999").as("ExpectedRequest")
         cy.visit('/events/find')
-        cy.wait("@EventsRequest")
-
         cy.get("[data-cy='keyword search']").type("test")
         cy.get("[data-cy='zip']").type("99999")
         cy.get("[data-cy='date']").as("DateRange").click()
         cy.get("@DateRange").find(".Select-menu-outer").contains("Tomorrow").click()
         cy.get("[data-cy='distance']").as("Distance").click()
         cy.get("@Distance").find(".Select-menu-outer").contains("50 miles").click()
-
         cy.get("[data-cy='search button']").click()
-
-        cy.wait("@EventsRequest").then((xhr) => {
-            expect(xhr.url).to.contain("pageSize=5")
-            expect(xhr.url).to.contain("start=0")
-            expect(xhr.url).to.contain("q=test")
-            expect(xhr.url).to.contain("address=9999")
-            expect(xhr.url).to.contain("dateRange=tomorrow")
-            expect(xhr.url).to.contain("distance=50")
-            
-        })
+        cy.wait("@ExpectedRequest")
     })
 
     it("has a date range filter with options", function(){
@@ -134,11 +123,12 @@ describe("Events Find Page", function () {
         cy.get("[data-cy='date']").as("DateRange")
         cy.get("@DateRange").click()
         cy.get(".Select-menu-outer").as("DateRangeOptions")
-        cy.get("@DateRangeOptions").contains("All Upcoming").should("exist")
-        cy.get("@DateRangeOptions").contains("Today").should("exist")
-        cy.get("@DateRangeOptions").contains("Tomorrow").should("exist")
-        cy.get("@DateRangeOptions").contains("Next 7 Days").should("exist")
-        cy.get("@DateRangeOptions").contains("Next 30 Days").should("exist")
+
+        cy.get("@DateRangeOptions").contains("All Upcoming")
+        cy.get("@DateRangeOptions").contains("Today")
+        cy.get("@DateRangeOptions").contains("Tomorrow")
+        cy.get("@DateRangeOptions").contains("Next 7 Days")
+        cy.get("@DateRangeOptions").contains("Next 30 Days")
     })
 
     it("has a miles dropdown with options", function(){
@@ -147,10 +137,11 @@ describe("Events Find Page", function () {
         cy.get("[data-cy='distance']").as("Distance")
         cy.get("@Distance").click()
         cy.get("@Distance").find(".Select-menu-outer").as("DistanceOptions")
-        cy.get("@DistanceOptions").contains("200 miles").should("exist")
-        cy.get("@DistanceOptions").contains("100 miles").should("exist")
-        cy.get("@DistanceOptions").contains("50 miles").should("exist")
-        cy.get("@DistanceOptions").contains("25 miles").should("exist")
+
+        cy.get("@DistanceOptions").contains("200 miles")
+        cy.get("@DistanceOptions").contains("100 miles")
+        cy.get("@DistanceOptions").contains("50 miles")
+        cy.get("@DistanceOptions").contains("25 miles")
     })
 
     it("has a distance default of 200 miles", function() {
@@ -163,25 +154,97 @@ describe("Events Find Page", function () {
         cy.get("[data-cy='distance']").find(".Select-value").should("have.text", "200 miles")
     })
 
-    it.only("displays results when they exist", function(){
+    describe("event results", function(){
+        it("has a date", function(){
+            cy.server()
+            cy.fixture("event/search-results.json").as("EventResults").then((event) => {
+                event.items[0].startDate = "2019-02-28T23:30:00-08:00"
+                cy.route("GET", "/api/content/events.json**", "@EventResults")
+            })
+            const expectedDate = "Thursday, February 28"
+            cy.visit("/events/find")
+            cy.get("[data-cy='event result']").eq(0).find("[data-cy='date']").should("have.text", expectedDate)
+        })
 
-        const expectedDate = "Thursday, February 28"
-        const expectedTime = "11:30 pm–1:30 am"
-        const expectedTitle = "Where’s the Contract? How To Do Business with the General Services Administration (GSA)"
-        const expectedLocation = "Las Vegas, Nevada"
-        const expectedCost = "Free"
+        it("has a time", function(){
+            cy.server()
+            cy.fixture("event/search-results.json").as("EventResults").then((event) => {
+                event.items[0].startDate = "2019-02-28T23:30:00-08:00"
+                event.items[0].endDate = "2019-03-01T01:30:00-08:00"
+                event.items[0].timezone = "PST"
+                cy.route("GET", "/api/content/events.json**", "@EventResults")
+            })
+            const expectedTime = "11:30 pm–1:30 am PST"
+            cy.visit("/events/find")
+            cy.get("[data-cy='event result']").eq(0).find("[data-cy='time']").should("have.text", expectedTime)
+        })
 
-        cy.server()
-        cy.fixture("event/search-results.json").as("EventResults")
-        cy.route("GET", "/api/content/events.json**", "@EventResults")
+        it("has a title", function(){
+            cy.server()
+            cy.fixture("event/search-results.json").as("EventResults").then((event) => {
+                event.items[0].title = "Chuck Norris Business Classes (now with more roundhouse)"
+                cy.route("GET", "/api/content/events.json**", "@EventResults")
+            })
+            const expectedTitle = "Chuck Norris Business Classes (now with more roundhouse)"
+            cy.visit("/events/find")
+            cy.get("[data-cy='event result']").eq(0).find("[data-cy= 'title']").should("have.text", expectedTitle)
+        })
 
-        cy.visit("/events/find")
-        cy.get("[data-cy='event result']").eq(0).find("[data-cy= 'date']").should("equal", expectedDate)
-        cy.get("[data-cy='event result']").eq(0).find("[data-cy= 'time']").should("have.text", expectedTime)
-        cy.get("[data-cy='event result']").eq(0).find("[data-cy= 'title']").should("equal", expectedTitle)
-        cy.get("[data-cy='event result']").eq(0).find("[data-cy= 'location']").should("equal", expectedLocation)
-        cy.get("[data-cy='event result']").eq(0).find("[data-cy= 'cost']").should("equal", expectedCost)
-        cy.get("[data-cy='event result']").eq(0).find("[data-cy= 'registration']")
+        it("has a location", function(){
+            cy.server()
+            cy.fixture("event/search-results.json").as("EventResults").then((event) => {
+                event.items[0].location.city = "Deep In The Heart O"
+                event.items[0].location.state = "Texas"
+                cy.route("GET", "/api/content/events.json**", "@EventResults")
+            })
+            const expectedLocation = "Deep In The Heart O, Texas"
+            cy.visit("/events/find")
+            cy.get("[data-cy='event result']").eq(0).find("[data-cy= 'location']").should("have.text", expectedLocation)
+        })
+
+        it("has a cost with a dollar value", function(){
+            cy.server()
+            cy.fixture("event/search-results.json").as("EventResults").then((event) => {
+                event.items[0].cost = "13.37"
+                cy.route("GET", "/api/content/events.json**", "@EventResults")
+            })
+            const expectedCost = "$13.37"
+            cy.visit("/events/find")
+            cy.get("[data-cy='event result']").eq(0).find("[data-cy='cost']").should("have.text", expectedCost)
+        })
+
+        it("has a cost when free", function(){
+            cy.server()
+            cy.fixture("event/search-results.json").as("EventResults").then((event) => {
+                event.items[0].cost = "0.00"
+                cy.route("GET", "/api/content/events.json**", "@EventResults")
+            })
+            const expectedCost = "Free"
+            cy.visit("/events/find")
+            cy.get("[data-cy='event result']").eq(0).find("[data-cy='cost']").should("have.text", expectedCost)
+        })
+
+        it("has a registration link with the label register", function(){
+            cy.server()
+            cy.fixture("event/search-results.json").as("EventResults").then((event) => {
+                event.items[0].registrationUrl = "https://doesnt.matter"
+                cy.route("GET", "/api/content/events.json**", "@EventResults")
+            })
+            const expectedRegistrationLabel = "REGISTER"
+            cy.visit("/events/find")
+            cy.get("[data-cy='event result']").eq(0).find("[data-cy='registration']").should("have.text", expectedRegistrationLabel)
+        })
+
+        // ToDo:  enable this test later on when this feature is enabled
+        it.skip("has no registration button with no registration url", function(){
+            cy.server()
+            cy.fixture("event/search-results.json").as("EventResults").then((event) => {
+                event.items[0].registrationUrl = ""
+                cy.route("GET", "/api/content/events.json**", "@EventResults")
+            })
+            cy.visit("/events/find")
+            expect(cy.get("[data-cy='event result']").eq(0).find("[data-cy='registration']")).not.to.exist
+        })
 
     })
 
@@ -225,7 +288,6 @@ describe("Events Find Page", function () {
 
         cy.visit("events/find")
         cy.get("[data-cy= 'event result']").should('have.length', 10)
-
     })
 
 })
