e476ae86d553d8bcd264cd88fbd62cde7be5ece1
Aaron Brooks <brooksa321@gmail.com>
2019-03-05 14:25:43 -0500

    updated event description cypress tests

--- cypress/integration/tests/event-page.test.js
+++ cypress/integration/tests/event-page.test.js
@@ -1,10 +1,11 @@
 describe('Event page', () => {
-    let firstEventId, firstEventTitle;
+    let firstEventId, firstEventTitle, firstEventDescription;
 
     before(() => {
         cy.request("/api/content/events.json").then(response => {
-            firstEventId = response.body[0].id
-            firstEventTitle = response.body[0].title
+            firstEventId = response.body.items[0].id
+            firstEventTitle = response.body.items[0].title
+            firstEventDescription = response.body.items[0].description
             cy.log("Found event id: " + firstEventId)
         });
 
@@ -25,5 +26,20 @@ describe('Event page', () => {
         cy.visit(testUrlBase + firstEventId)
         cy.get("[data-cy='event-title']").as("EventTitle").should('exist')
         cy.get("@EventTitle").should("have.text", expectedTitle)
+    })
+
+    it('should load and have a description', function() {
+        cy.visit(testUrlBase + firstEventId)
+        cy.get("[data-cy='event-description']").should("have.text", firstEventDescription)
+    })
+
+    it('displays a description for an event', () => {
+        cy.server()
+        cy.fixture("event/event.json").as("EventDetailResult")
+        cy.route("GET", "/api/content/event/" + firstEventId + ".json", "@EventDetailResult").as("EventDetailRoute")
+        const expectedDetails = "This is an event description"
+        cy.visit(testUrlBase + firstEventId)
+        cy.get("[data-cy='event-description']").as("EventDetails").should('exist')
+        cy.get("@EventDetails").should("have.text", expectedDetails)
     })
 })
