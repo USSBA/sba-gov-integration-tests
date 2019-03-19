9a67d2cc67f358b766c644ad02d91073609bdf38
Alex de los Reyes <cyberdecker@gmail.com>
2019-03-19 19:02:57 -0400

    TA-1574 Restructing details tests and adding a fixture

--- cypress/integration/tests/event-page.test.js
+++ cypress/integration/tests/event-page.test.js
@@ -1,80 +1,123 @@
-const mockEventData = require('../../fixtures/event/event.json')
+describe("Event details page", function(){
 
-describe('Event page', () => {
-    let firstEventId, firstEventTitle, firstEventRegistrationUrl, firstEventDescription, 
-
-    before(() => {
-        cy.request("/api/content/events.json").then(response => {
-            firstEventId = response.body.items[0].id
-            firstEventTitle = response.body.items[0].title
-            firstEventDescription = response.body.items[0].description
-            firstEventCost = response.body.items[0].cost
-            firstEventLocation = response.body.items[0].location
-            cy.log("Found event id: " + firstEventId)
-        });
+    beforeEach(function(){
+        cy.fixture("event/99999.json").as("event")
     })
 
-    let testUrlBase = "/event/"
-
-    it('should load and have a title', function() {
-        cy.visit(testUrlBase + firstEventId)
-        cy.get("head > title").should("have.text", firstEventTitle)
-    })
-
-    it('displays a title for an event', () => {
+    it("has all the event details", function(){
         cy.server()
-        cy.fixture("event/event.json").as("EventDetailResult")
-        cy.route("GET", "/api/content/event/" + firstEventId + ".json", "@EventDetailResult").as("EventDetailRoute")
-        const expectedTitle = "Event Title"
-        cy.visit(testUrlBase + firstEventId)
-        cy.get("[data-cy='event-title']").as("EventTitle").should('exist')
-        cy.get("@EventTitle").should("have.text", expectedTitle)
+        cy.route("GET", "/api/content/event/99999.json", "@event").as("TestEventDetails")
+        cy.visit("/event/99999")
+
+        const expectedDescriptionLabel = "Description"
+        const expectedHeaderDate = "Tuesday, March 19"
+        const expectedDate = "Tuesday, March 19, 2019"
+        const expectedRecurringText = "Recurs weekly"
+        const expectedTime = "5am-12pm " + this.event.timezone
+
+        cy.get("[data-cy='event-title']").should("have.text", this.event.title)
+        cy.get("[data-cy='event-header-date']").should("have.text", expectedHeaderDate)
+        cy.get("[data-cy='event-details-date']").should("have.text", expectedDate)
+        cy.get("[data-cy='event-details-time']").should("have.text", expectedTime)
+        cy.get("[data-cy='event-description-label']").should("have.text", expectedDescriptionLabel)
+        cy.get("[data-cy='event-description']").should('have.text', this.event.description)
+        cy.get("[data-cy='event-details-cost']").should('have.text', "$"+this.event.cost)
+        cy.get("[data-cy='event-details-recurring']").should('have.text', expectedRecurringText)
+        cy.get("[data-cy='event-details-location']")
+            .should('contain', this.event.location.name)
+            .and('contain', this.event.location.address)
+            .and('contain', this.event.location.address_additional)
+            .and('contain', this.event.location.city)
+            .and('contain', this.event.location.zipcode)
+            .and('contain', this.event.location.state)
+        cy.get("a#event-details-location-link").should("have.text", "View on Map")
+        cy.get("[data-cy=event-details-organizer]").should('have.text', this.event.contact.name)
+        cy.get('[data-cy=email]').should('have.text', this.event.contact.email)
+        cy.get('[data-cy=event-details-phone]').should('have.text', this.event.contact.phone)
     })
 
-    it('should load and have a description', function() {
-        cy.visit(testUrlBase + firstEventId)
-        cy.get("[data-cy='event-description']").should("have.text", firstEventDescription)
+    it("displays free when there is no cost", function(){
+        cy.server()
+        cy.fixture("event/99999.json").as("specialEvent").then(event => {
+            event.cost = "0.00"
+            cy.route("GET", "/api/content/event/99999.json", "@specialEvent").as("TestEventDetails")
+        })
+        cy.visit("/event/99999")
+        cy.get("[data-cy=event-details-cost").should("have.text", "Free")
     })
 
-    it('displays a description for an event', () => {
+    it("displays minutes in the time", function(){
         cy.server()
-        cy.fixture("event/event.json").as("EventDetailResult")
-        cy.route("GET", "/api/content/event/" + firstEventId + ".json", "@EventDetailResult").as("EventDetailRoute")
-        const expectedDetails = "This is an event description"
-        cy.visit(testUrlBase + firstEventId)
-        cy.get("[data-cy='event-description']").as("EventDetails").should('exist')
-        cy.get("@EventDetails").should("have.text", expectedDetails)
+        cy.fixture("event/99999.json").as("specialEvent").then(event => {
+            event.startDate =  "2019-03-19T05:55:00-04:00"
+            event.endDate = "2019-03-19T12:12:00-04:00"
+            event.timezone = "PST"
+            cy.route("GET", "/api/content/event/99999.json", "@specialEvent").as("TestEventDetails")
+        })
+        const expectedTime = "5:55am-12:12pm PST" 
+
+        cy.visit("/event/99999")
+        cy.get("[data-cy='event-details-time']").should("have.text", expectedTime)
     })
 
-    it('displays cost in details box', () => {
+    it("displays both am and pm in the time", function(){
         cy.server()
-        cy.fixture("event/event.json").as("EventDetailResult")
-        cy.route("GET", "/api/content/event/" + firstEventId + ".json", "@EventDetailResult").as("EventDetailRoute")
-        const expectedCost = "$25.00"
-        cy.visit(testUrlBase + firstEventId)
-        cy.get("[data-cy='event-details-cost']").as("EventCost").should('exist')
-        cy.get("@EventDetails").should("have.text", expectedCost)
+        cy.fixture("event/99999.json").as("specialEvent").then(event => {
+            event.startDate =  "2019-03-19T17:00:00-04:00"
+            event.endDate = "2019-03-19T20:00:00-04:00"
+            event.timezone = "PST"
+            cy.route("GET", "/api/content/event/99999.json", "@specialEvent").as("TestEventDetails")
+        })
+        const expectedTime = "5-8pm PST" 
 
-    it('displays cost in details box', () => {
-        cy.server()
-        cy.fixture("event/event.json").as("EventDetailResult")
-        cy.route("GET", "/api/content/event/" + firstEventId + ".json", "@EventDetailResult").as("EventDetailRoute")
-        const expectedCost = "Fayetteville State University1200 Murchison Road, Fayetteville, North Carolina 28301View on Map"
-        cy.visit(testUrlBase + firstEventId)
-        cy.get("[data-cy='event-details-location']").as("EventLocation").should('exist')
-        cy.get("@EventDetails").should("have.text", expectedLocation)
+        cy.visit("/event/99999")
+        cy.get("[data-cy='event-details-time']").should("have.text", expectedTime)
+    })
 
     it("should display leaving sba modal when registration button is clicked", function(){
         cy.server()
-        const expectedUrl = mockEventData.registrationUrl
-        cy.fixture("event/event.json").as("EventDetailResult")
-        cy.route("GET", "/api/content/event/" + firstEventId + ".json", "@EventDetailResult").as("EventDetailRoute")
-        cy.visit(testUrlBase + firstEventId)
-        cy.get('[data-cy="registration"]').should("exist")
+        const expectedUrl = this.event.registrationUrl
+        cy.route("GET", "/api/content/event/99999.json", "@event").as("EventDetailRoute")
+        cy.visit("/event/99999")
+        cy.get('[data-cy="registration"]')
         cy.get('button').contains("REGISTER").click()
         cy.get("[data-cy='external url']").should("have.text", expectedUrl)
     })
+
+    it("should display details for an online event", function(){
+        cy.server()
+        cy.fixture("event/99999.json").as("specialEvent").then(event => {
+            event.locationType =  "Online"
+            cy.route("GET", "/api/content/event/99999.json", "@specialEvent").as("TestEventDetails")
+        })
+        cy.visit("/event/99999")
+        cy.get("#event-details-location").should("have.text", "Online")
+    })
+
+    it("should have a default for unknown recurring values", function(){
+        cy.server()
+        cy.fixture("event/99999.json").as("specialEvent").then(event => {
+            event.recurring =  "Yes"
+            event.recurringType = "Recurs semi bi annually"
+            cy.route("GET", "/api/content/event/99999.json", "@specialEvent").as("TestEventDetails")
+        })
+        const expectedRecurringText = "This is a recurring event"
+        cy.visit("/event/99999")
+        cy.get("[data-cy='event-details-recurring']").should('have.text', expectedRecurringText)
+    })
+
+    it("should not display recurring details when an event does not recur", function(){
+        cy.server()
+        cy.fixture("event/99999.json").as("specialEvent").then(event => {
+            event.recurring =  "No"
+            event.recurringType = "Recurs semi bi annually"
+            cy.route("GET", "/api/content/event/99999.json", "@specialEvent").as("TestEventDetails")
+        })
+        cy.visit("/event/99999")
+        cy.get("[data-cy='event-details-recurring']").should("not.exist")
+    })
 })
+
 describe('Event 404 page', () => {
   let testUrlBase = "/event/"
   it('displays the 404 page when the event is NOT found', function() {
