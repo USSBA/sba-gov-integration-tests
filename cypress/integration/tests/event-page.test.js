cb9eb19e4e763b014e6f578f24498c6c76d37e9c
Aaron Brooks <brooksa321@gmail.com>
2019-03-19 15:28:45 -0400

    Added test for description cost and location

--- cypress/integration/tests/event-page.test.js
+++ cypress/integration/tests/event-page.test.js
@@ -1,12 +1,15 @@
 const mockEventData = require('../../fixtures/event/event.json')
 
 describe('Event page', () => {
-    let firstEventId, firstEventTitle, firstEventRegistrationUrl
+    let firstEventId, firstEventTitle, firstEventRegistrationUrl, firstEventDescription, 
 
     before(() => {
         cy.request("/api/content/events.json").then(response => {
             firstEventId = response.body.items[0].id
             firstEventTitle = response.body.items[0].title
+            firstEventDescription = response.body.items[0].description
+            firstEventCost = response.body.items[0].cost
+            firstEventLocation = response.body.items[0].location
             cy.log("Found event id: " + firstEventId)
         });
     })
@@ -27,6 +30,39 @@ describe('Event page', () => {
         cy.get("[data-cy='event-title']").as("EventTitle").should('exist')
         cy.get("@EventTitle").should("have.text", expectedTitle)
     })
+
+    it('should load and have a description', function() {
+        cy.visit(testUrlBase + firstEventId)
+        cy.get("[data-cy='event-description']").should("have.text", firstEventDescription)
+    })
+
+    it('displays a description for an event', () => {
+        cy.server()
+        cy.fixture("event/event.json").as("EventDetailResult")
+        cy.route("GET", "/api/content/event/" + firstEventId + ".json", "@EventDetailResult").as("EventDetailRoute")
+        const expectedDetails = "This is an event description"
+        cy.visit(testUrlBase + firstEventId)
+        cy.get("[data-cy='event-description']").as("EventDetails").should('exist')
+        cy.get("@EventDetails").should("have.text", expectedDetails)
+    })
+
+    it('displays cost in details box', () => {
+        cy.server()
+        cy.fixture("event/event.json").as("EventDetailResult")
+        cy.route("GET", "/api/content/event/" + firstEventId + ".json", "@EventDetailResult").as("EventDetailRoute")
+        const expectedCost = "$25.00"
+        cy.visit(testUrlBase + firstEventId)
+        cy.get("[data-cy='event-details-cost']").as("EventCost").should('exist')
+        cy.get("@EventDetails").should("have.text", expectedCost)
+
+    it('displays cost in details box', () => {
+        cy.server()
+        cy.fixture("event/event.json").as("EventDetailResult")
+        cy.route("GET", "/api/content/event/" + firstEventId + ".json", "@EventDetailResult").as("EventDetailRoute")
+        const expectedCost = "Fayetteville State University1200 Murchison Road, Fayetteville, North Carolina 28301View on Map"
+        cy.visit(testUrlBase + firstEventId)
+        cy.get("[data-cy='event-details-location']").as("EventLocation").should('exist')
+        cy.get("@EventDetails").should("have.text", expectedLocation)
 
     it("should display leaving sba modal when registration button is clicked", function(){
         cy.server()
