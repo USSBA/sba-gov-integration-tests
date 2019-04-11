4a337338f5dccfe0756f09761eab32c9c646963e
Brian Le <brian.mk.le@gmail.com>
2019-04-11 10:51:25 -0400

    T1-921 Add more tests for person lookup page

--- cypress/integration/tests/person-lookup-page.test.js
+++ cypress/integration/tests/person-lookup-page.test.js
@@ -1,18 +1,16 @@
 describe('Person lookup page', function () {
-  const defaultOffice = 'all'
-  const defaultOrder = 'ascending'
-  const defaultPageNumber = 1
-  const defaultPageSize = 12
-
   beforeEach(function () {
     cy.fixture('persons/persons.json').as('persons')
   })
 
   it('has fields for filtering and sorting', function () {
-    // handle redirect without failing test
-    cy.visit('/person', { failOnStatusCode: false })
+    cy.server()
+    cy.route('GET', '/api/content/persons.json', '@persons')
+
+    cy.visit('/person')
 
     cy.get('label[for="search"]').should('have.text', 'Search')
+    cy.get('[data-cy="search"]')
 
     cy.get('label[for="office"]').should('have.text', 'Office')
     cy.get('[data-cy="office"]')
@@ -24,9 +22,9 @@ describe('Person lookup page', function () {
       .find('.Select-menu-outer')
       .should('contain', 'All')
       // first office
-      .and('contain', 'Alabama District Office')
+      .and('contain', 'Dallas/Fort Worth District Office')
       // last office
-      .and('contain', 'Wyoming District Office')
+      .and('contain', 'St. Louis District Office')
 
     cy.get('label[for="sort-by"]')
     cy.get('[data-cy="sort-by"]')
@@ -42,39 +40,172 @@ describe('Person lookup page', function () {
     cy.get('[data-cy="search button"]')
   })
 
-  it('should push query parameters to the url on search (without any selections)', function () {
-    cy.visit('/person', { failOnStatusCode: false })
+  it('pushes query parameters to the url on search (without any selections)', function () {
+    cy.visit('/person')
 
     cy.get('[data-cy="search button"]').click()
 
     cy.url()
-      .should('contain', `pageNumber=${defaultPageNumber}`)
-      .should('contain', `pageSize=${defaultPageSize}`)
-      .should('contain', `office=${defaultOffice}`)
-      .should('contain', `order=${defaultOrder}`)
+      .should('contain', `pageNumber=1`)
+      .should('contain', `pageSize=12`)
+      .should('contain', `office=all`)
+      .should('contain', `order=ascending`)
   })
 
-  it(`displays ${defaultPageSize} cards with person details by default`, function () {
+  it(`displays 12 cards with person details by default`, function () {
     cy.server()
     cy.route('GET', '/api/content/persons.json', '@persons')
 
-    cy.visit('/person', { failOnStatusCode: false })
+    cy.visit('/person')
 
-    cy.get('[class~=document-card-collection]')
-      .find('[class~=card-container]')
-      .should('have.length', defaultPageSize)
+    cy.get('[data-cy="detail-card-collection-container"]')
+      .should('have.length', 12)
   })
 
-  it('should open person detail page url when person name or "See bio" is clicked', function () {
+  it('opens the person detail page url when the person\'s name is clicked', function () {
     cy.server()
     cy.route('GET', '/api/content/persons.json', '@persons')
 
-    cy.visit('/person', { failOnStatusCode: false })
-
-    cy.get('[class~=detail-card]').eq(0).find('h6 > a').click()
+    cy.visit('/person')
 
     cy.get('@persons').then(persons => {
+      cy.get('[data-cy="detail-card"]')
+        .first()
+        .contains(persons[0].name)
+        .click()
+
       cy.url().should('contain', `${persons[0].url}`)
+    })
+  })
+
+  it('displays details about a person for a result', function () {
+    const expectedName = 'Sabrina Abousaleh'
+    const expectedTitle = 'Administrative Officer'
+    const expectedOffice = 'Nevada District Office'
+    const expectedPhone = '702-388-6683'
+    const expectedEmail = 'sabrina.abousaleh@sba.gov'
+
+    cy.server()
+    cy.route('GET', '/api/content/persons.json', '@persons')
+
+    cy.visit('/person')
+
+    cy.get('[data-cy="detail-card"]')
+      .eq(2)
+      .as('thirdCard')
+
+    cy.get('@thirdCard').find('[data-cy="detail-card-title"]').should('have.text', expectedName)
+    cy.get('@thirdCard').find('[data-cy="person title"]').should('have.text', expectedTitle)
+    cy.get('@thirdCard').find('[data-cy="person office"]').should('have.text', expectedOffice)
+    cy.get('@thirdCard').find('[data-cy="contact phone"]').should('have.text', expectedPhone)
+    cy.get('@thirdCard').find('[data-cy="contact email"]').should('have.text', expectedEmail)
+  })
+
+  it('displays a message when no search results are found', function () {
+    cy.server()
+    cy.route('GET', '/api/content/persons.json', [])
+    cy.visit('person')
+
+    cy.get('[data-cy="no-results"]').should('exist')
+  })
+
+  it('searches and displays people with first and/or last names matching the search term', function () {
+    const firstExpectedName = 'Gary Alexander'
+    const secondExpectedName = 'Alex Kohls'
+
+    cy.server()
+    cy.route('GET', '/api/content/persons.json', '@persons')
+
+    cy.visit('/person')
+
+    cy.get('[data-cy="search"]').type('alex')
+    cy.get('[data-cy="search button"]').click()
+
+    cy.get('[data-cy="detail-card"]').as('detailCards')
+
+    cy.get('@detailCards')
+      .first()
+      .should('contain', firstExpectedName)
+
+    cy.get('@detailCards')
+      .eq(1)
+      .find('[data-cy="detail-card-title"]')
+      .should('contain', secondExpectedName)
+  })
+
+  it('searches and displays people with offices matching the search term', function () {
+    const expectedOffice = 'St. Louis District Office'
+
+    cy.server()
+    cy.route('GET', '/api/content/persons.json', '@persons')
+
+    cy.visit('/person')
+
+    cy.get('[data-cy="search"]').type('louis')
+    cy.get('[data-cy="search button"]').click()
+
+    cy.get('[data-cy="detail-card"]').as('detailCards')
+      .first()
+      .should('contain', expectedOffice)
+  })
+
+  it('filters people by office name when an office name is selected', function () {
+    const expectedCount = 2
+
+    cy.server()
+    cy.route('GET', '/api/content/persons.json', '@persons')
+
+    cy.visit('/person')
+
+    cy.get('[data-cy="office"]')
+      .as('office')
+      // Grab text input from React-Select dropdown
+      .find('.Select-input > input')
+      // React-Select covers the input with a <span> to hide it
+      .type('Pittsburgh District Office', { force: true })
+
+    // Select first option that should be labelled "Pittsburgh District Office"
+    cy.get('@office')
+      .find('.Select-option')
+      .click()
+
+    cy.get('[data-cy="search button"]').click()
+
+    cy.get('[data-cy="detail-card"]').as('detailCards')
+      .should('have.length', expectedCount)
+  })
+
+  it('displays people in descending last name order when Name Z-A is selected', function () {
+    cy.server()
+    cy.route('GET', '/api/content/persons.json', '@persons')
+
+    cy.visit('/person')
+
+    cy.get('@persons').then(persons => {
+      const firstPersonName = persons[0].name
+      const lastPersonName = persons[persons.length - 1].name
+
+      cy.get('[data-cy="detail-card"]')
+        .first()
+        .as('firstCard')
+
+      cy.get('@firstCard').should('contain', firstPersonName)
+
+      // Click arrow in React-Select dropdown to show options
+      cy.get('[data-cy="sort-by"]')
+        .as('sortBy')
+        .find('.Select-arrow-zone')
+        .click()
+
+      // Select second option (Name Z-A)
+      cy.get('@sortBy')
+        .find('.Select-option')
+        .eq(1)
+        .click()
+
+      cy.get('[data-cy="search button"]').click()
+
+      cy.get('@firstCard').should('contain', lastPersonName)
     })
   })
 })
