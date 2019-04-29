0e936cdc25dbb25b5c3b6f231902c4513f7dedb0
Amy DiPasquale <ald5044@gmail.com>
2019-04-29 17:34:44 -0400

    TA-2278 consolidate tests

--- cypress/integration/tests/blogs-landing.test.js
+++ cypress/integration/tests/blogs-landing.test.js
@@ -4,7 +4,7 @@ describe("Blogs landing page", function() {
     cy.get("[data-testid=blogs-hero]")
   })
 
-  it('displays both category titles and subtitles', function() {
+  it('displays category information for both categories', function() {
     cy.server()
     cy.fixture("blogs/news-and-views-blogs.json").as("NewsAndViewsBlogs").then(() => {
       cy.route("GET", "/api/content/search/blogs.json?category=News and Views&end=3&order=desc", "@NewsAndViewsBlogs")
@@ -13,32 +13,30 @@ describe("Blogs landing page", function() {
       cy.route("GET", "/api/content/search/blogs.json?category=Industry Word&end=3&order=desc", "@IndustryWordBlogs")
     })
 
-    const firstCategory = {
+    const newsAndViewsCategory = {
       title: 'SBA News & Views posts',
       subtitle: "Insights and updates from SBA's small business experts."
     }
 
-    const secondCategory = {
+    const industryWordCategory = {
       title: 'Industry Word posts',
       subtitle: "Commentary and advice from leaders in the small business industry."
     }
 
     cy.visit("/blogs/")
-    cy.get("[data-testid=category-title]").eq(0).should("have.text", firstCategory.title)
-    cy.get("[data-testid=category-subtitle]").eq(0).should("have.text", firstCategory.subtitle)
-    cy.get("[data-testid=category-title]").eq(1).should("have.text", secondCategory.title)
-    cy.get("[data-testid=category-subtitle]").eq(1).should("have.text", secondCategory.subtitle)
-  })
 
-  it('displays three blogs for a category', function() {
-    cy.server()
-    cy.fixture("blogs/news-and-views-blogs.json").as("NewsAndViewsBlogs").then(() => {
-      cy.route("GET", "/api/content/search/blogs.json?category=News and Views&end=3&order=desc", "@NewsAndViewsBlogs")
+    cy.get("[data-testid='SBA News & Views posts']").within((NewsAndViewsCategory) => {
+      cy.get("[data-testid=category-title]").should("have.text", newsAndViewsCategory.title)
+      cy.get("[data-testid=category-subtitle]").should("have.text", newsAndViewsCategory.subtitle)
+      cy.get("[data-testid=card]").should('have.length', 3)
+      cy.get("[data-testid='see more button']").should("have.text", "SEE MORE POSTS")
     })
 
-    cy.visit("/blogs/")
-
-    cy.get("[data-testid='SBA News & Views posts']").as("NewsAndViewsCategory")
-      .find("[data-testid=card]").should('have.length', 3)
+    cy.get("[data-testid='Industry Word posts']").within((IndustryWordCategory) => {
+      cy.get("[data-testid=category-title]").should("have.text", industryWordCategory.title)
+      cy.get("[data-testid=category-subtitle]").should("have.text", industryWordCategory.subtitle)
+      cy.get("[data-testid=card]").should('have.length', 3)
+      cy.get("[data-testid='see more button']").should("have.text", "SEE MORE POSTS")
+    })
   })
 })
