4cfbad099b5b7f03f6c4b849d6230629914ce2d2
amyd11 <ald5044@gmail.com>
2019-04-30 18:37:30 -0400

    TA-2278 blog landing categories (#41)
    * TA-2278 tests category title, subtitle, number of cards
    
    * TA-2278 consolidate tests
    
    * TA-2278 add test for blog card info
    
    * TA-2278 Adding a blog test tool
    
    * TA-2278 combining a test and adding url check
    
    * TA-2300 Tweaks to author card image tests (#42)
    
    * TA-2300 update to handle blog search endpoint (#43)
    
    * TA-2278 Combines hero test into main test
    
    * TA-2278 Modifying blog mock structure
    
    * TA-2278 tests category title, subtitle, number of cards
    
    * TA-2278 consolidate tests
    
    * TA-2278 add test for blog card info
    
    * TA-2278 Adding a blog test tool
    
    * TA-2278 combining a test and adding url check
    
    * TA-2278 Combines hero test into main test
    
    * TA-2278 Modifying blog mock structure

---
+++ cypress/integration/tests/blogs-landing.test.js
@@ -0,0 +1,62 @@
+import moment from 'moment'
+
+describe("Blogs landing page", function() {
+  beforeEach(function() {
+    cy.fixture("blogs/news-and-views-blogs.json").as("NewsAndViewsBlogs")
+    cy.fixture("blogs/industry-word-blogs.json").as("IndustryWordBlogs")
+  })
+  
+  it('displays all landing page elements', function() {
+    cy.server()
+    cy.route("GET", "/api/content/search/blogs.json?category=News and Views&end=3&order=desc", this.NewsAndViewsBlogs).as("NewsAndViewsRequest")
+    cy.route("GET", "/api/content/search/blogs.json?category=Industry Word&end=3&order=desc", this.IndustryWordBlogs).as("IndustryWordRequest")
+    
+    const newsAndViewsCategory = {
+      title: 'SBA News & Views posts',
+      subtitle: "Insights and updates from SBA's small business experts."
+    }
+    
+    const industryWordCategory = {
+      title: 'Industry Word posts',
+      subtitle: "Commentary and advice from leaders in the small business industry."
+    }
+    
+    cy.visit("/blogs/")
+    cy.wait("@NewsAndViewsRequest")
+    cy.wait("@IndustryWordRequest")
+
+    cy.get("[data-testid=blogs-hero]")
+    
+    cy.get("[data-testid='SBA News & Views posts']").within((NewsAndViewsCategory) => {
+      cy.get("[data-testid=category-title]").should("have.text", newsAndViewsCategory.title)
+      cy.get("[data-testid=category-subtitle]").should("have.text", newsAndViewsCategory.subtitle)
+      cy.get("[data-testid=card]").should('have.length', 3)
+      cy.get("[data-testid='see more button']").should("have.text", "SEE MORE POSTS")
+      cy.get("[data-testid='see more button']").should("have.attr","href", "/blogs/news-and-views")
+    })
+
+    cy.get("[data-testid='Industry Word posts']").within((IndustryWordCategory) => {
+      cy.get("[data-testid=category-title]").should("have.text", industryWordCategory.title)
+      cy.get("[data-testid=category-subtitle]").should("have.text", industryWordCategory.subtitle)
+      cy.get("[data-testid=card]").should('have.length', 3)
+      cy.get("[data-testid='see more button']").should("have.text", "SEE MORE POSTS")
+      cy.get("[data-testid='see more button']").should("have.attr","href", "/blogs/industry-word")
+    })
+  })
+      
+  it("displays blog information on the card", function() {
+    cy.server()
+    cy.route("GET", "/api/content/search/blogs.json?category=News and Views&end=3&order=desc", "@NewsAndViewsBlogs").as("NewsAndViewsRequest")
+    cy.visit("/blogs/")
+    cy.wait("@NewsAndViewsRequest")
+
+    cy.get('[data-testid="card"]').eq(0).as("Card1").within(function(FirstBlogCard) {
+      const formattedDate = moment.unix(this.NewsAndViewsBlogs.blogs[0].created).format('MMMM D, YYYY')
+
+      cy.get('[data-testid="card title"]').should("have.text", this.NewsAndViewsBlogs.blogs[0].title)
+      cy.get('[data-testid="card italic text"]').should("have.text", formattedDate)
+      cy.get('[data-testid="card subtitle text"]').should("have.text", this.NewsAndViewsBlogs.blogs[0].summary)
+      cy.get('[data-testid="card link"]').should("have.text", "Read full post")
+    })
+  })
+})
