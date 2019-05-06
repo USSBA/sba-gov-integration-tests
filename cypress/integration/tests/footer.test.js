842a1504ef1ecf48ec4bae6163012e0220d2b575
Kevin Lee <ki.lee@sba.gov>
2019-05-06 12:34:27 -0400

    T1-1331 Add more govdelivery tests in footer

--- cypress/integration/tests/footer.test.js
+++ cypress/integration/tests/footer.test.js
@@ -1,15 +1,27 @@
 describe("Footer", function () {
-  it("has GovDelivery form", function() {
-    cy.visit("/")
-    cy.get("[data-testid=newsletter-form]").parent().parent().should('have.id', 'sba-footer')
-  })
+  describe("GovDelivery form", function() {
 
-  it("Subscribe button in GovDelivery redirects to /updates", function() {
-    // the elements in /updates page is tested in govdelivery.test.js file
-    const baseUrl = Cypress.config("baseUrl")
-    cy.visit("/")
-    cy.get("[data-testid=newsletter-form]").contains("Subscribe").click()
-    cy.url().should('include', '/updates')
-    cy.url().should('eq', `${baseUrl}/updates`)
+    it("renders correct elements", function() {
+      cy.visit("/")
+      cy.get("[data-testid=newsletter-form]").parent().parent().should('have.id', 'sba-footer')
+      cy.get("[data-testid=newsletter-form]")
+        .should("have.length", 1)
+      cy.get("[data-testid=newsletter-form]").contains("p", "Sign up for SBA email updates").should("exist")
+    })
+
+    it("doesn't render e-mail and zip code textboxes", function() {
+      cy.visit("/")
+      cy.get("#newsletter-email-address-container").should("not.exist")
+      cy.get("#newsletter-zip-code-container").should("not.exist")
+    })
+
+    it("Subscribe button redirects to /updates", function() {
+      // elements and functionalities in /updates page is tested in govdelivery.test.js file
+      const baseUrl = Cypress.config("baseUrl")
+      cy.visit("/")
+      cy.get("[data-testid=newsletter-form]").contains("Subscribe").click()
+      cy.url().should('include', '/updates')
+      cy.url().should('eq', `${baseUrl}/updates`)
+    })
   })
 })
