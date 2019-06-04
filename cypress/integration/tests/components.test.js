5a37941d6f24b15b4203f4ca1aac00251999bb2a
Alex de los Reyes <cyberdecker@gmail.com>
2019-06-04 15:53:49 -0400

    TA-2003 Adding tests for checking the exit modal accessibility

---
+++ cypress/integration/tests/components.test.js
@@ -0,0 +1,40 @@
+describe("Component", function () {
+    describe("exit modal", function () {
+
+        it("displays with accessible features", function () {
+            cy.server()
+            cy.route("GET", "/api/content/6134.json").as("HomepageContent")
+            cy.visit("/")
+            cy.wait("@HomepageContent")
+            cy.get("[alt='SBA Facebook page']")
+                .click()
+            cy.contains("You're leaving the Small Business Administration website.")
+            cy.get("[data-cy='close button']")
+                .should('have.attr', "aria-label", "Close this modal.")
+        })
+
+        it("closes with the escape button", function () {
+            cy.server()
+            cy.route("GET", "/api/content/6134.json").as("HomepageContent")
+            cy.visit("/")
+            cy.wait("@HomepageContent")
+            cy.get("[alt='SBA Facebook page']")
+                .click()
+            cy.contains("You're leaving the Small Business Administration website.")
+                .type('{esc}')
+            cy.contains("You're leaving the Small Business Administration website.").should("not.exist")
+        })
+
+        it("closes when the cancel button is activated", function () {
+            cy.server()
+            cy.route("GET", "/api/content/6134.json").as("HomepageContent")
+            cy.visit("/")
+            cy.wait("@HomepageContent")
+            cy.get("[alt='SBA Facebook page']")
+                .click()
+            cy.contains('CANCEL')
+                .click()
+            cy.contains("You're leaving the Small Business Administration website.").should("not.exist")
+        })
+    })
+})
