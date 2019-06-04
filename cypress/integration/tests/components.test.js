de164e62d82327ed1cd25795dc8ca8a4afa59ed2
Alex de los Reyes <cyberdecker@gmail.com>
2019-06-04 11:52:03 -0400

    TA-2491 Fixing tests and fixtures

--- cypress/integration/tests/components.test.js
+++ cypress/integration/tests/components.test.js
@@ -1,38 +1,44 @@
 describe("Component", function () {
-    describe("exit modal", function () {
+    describe("text section", function () {
+
         beforeEach(function () {
             cy.server()
             cy.fixture("basic-page/links.json").as("LinkPage")
             cy.route("GET", "/api/content/5.json", "@LinkPage").as("PageRequest")
         })
     
-        it("displays when there is a link to an external site", function () {
+        it("displays an exit modal for a link to an external site", function () {
             cy.visit('/business-guide/launch-your-business/choose-your-business-name')
             cy.wait("@PageRequest")
             cy.contains("External non-.gov link")
               .and('have.class', 'external-link-marker')
               .click()
-            // maybe need a better way to identify the modal?
             cy.contains("You're leaving the Small Business Administration website.")
         })
+
+        it("does not display an exit modal when the link is a relative link", function () {
+            cy.visit('/business-guide/launch-your-business/choose-your-business-name')
+            cy.wait("@PageRequest")
+            cy.contains("Internal relative link")
+              .and('not.have.class', 'external-link-marker')
+              .click()
+            cy.contains("You're leaving the Small Business Administration website.").should('not.exist')
+        })
     
-        it("does not display when there is a link to a .gov site", function () {
+        it("does not put an exit modal class on a link to a .gov site", function () {
             cy.visit('/business-guide/launch-your-business/choose-your-business-name')
             cy.wait("@PageRequest")
             cy.contains("External .gov link")
               .and('not.have.class', 'external-link-marker')
-              .click()
-            // maybe need a better way to identify the modal?
-            cy.contains("You're leaving the Small Business Administration website.").should('not.exist')
+            // exit modal should not display without an external-link-marker
         })
     
-        it("does not display when the link is a relative link", function () {
-          cy.visit('/business-guide/launch-your-business/choose-your-business-name')
-          cy.wait("@PageRequest")
-          cy.contains("Internal relative link")
-            .and('not.have.class', 'external-link-marker')
-            .click()
-          // maybe need a better way to identify the modal?
-          cy.contains("You're leaving the Small Business Administration website.").should('not.exist')    })
-      })
+        it("does not put an exit modal class on a link to a .gov site with a path", function () {
+            cy.visit('/business-guide/launch-your-business/choose-your-business-name')
+            cy.wait("@PageRequest")
+            cy.contains("External .gov link with path")
+              .and('not.have.class', 'external-link-marker')
+            // exit modal should not display without an external-link-marker
+        })
+    })
 })
