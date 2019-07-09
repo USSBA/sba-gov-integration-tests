5fc1a29696babdaaa4371fc38bda06f4e207690e
Alex de los Reyes <cyberdecker@gmail.com>
2019-07-09 15:25:22 -0400

    TA-2814 Adding some additional tests

--- cypress/integration/tests/translation.test.js
+++ cypress/integration/tests/translation.test.js
@@ -2,6 +2,7 @@ describe("Translated Pages", function () {
 
     beforeEach(function(){
         cy.fixture("program-page/translated-page.json").as("TranslatedPage")
+        cy.fixture("menu/translated.json").as("TranslatedMenu")
     })
 
     it("display spanish translation content on program pages for spanish urls", function () {
@@ -41,4 +42,37 @@ describe("Translated Pages", function () {
         cy.get("#section-header-0")
             .contains(this.TranslatedPage.paragraphs[0].text)
     })
+
+    it("display spanish menu when set to spanish", function () {
+        cy.server()
+        cy.route("GET","/api/content/search/mainMenu.json", this.TranslatedMenu).as("MenuRequest")
+        cy.visit("/?lang=es")
+        cy.wait("@MenuRequest")
+        cy.get("#sub-menu-0-title")
+            .contains(this.TranslatedMenu[0].spanishTranslation.linkTitle)
+            .should('have.attr', 'href', this.TranslatedMenu[0].spanishTranslation.link )
+    })
+
+    it("display english menu when set to english", function () {
+        cy.server()
+        cy.route("GET","/api/content/search/mainMenu.json", this.TranslatedMenu).as("MenuRequest")
+        cy.visit("/")
+        cy.wait("@MenuRequest")
+        cy.get("#sub-menu-0-title")
+            .contains(this.TranslatedMenu[0].linkTitle)
+            .should('have.attr', 'href', this.TranslatedMenu[0].link )
+    })
+
+    it("displays hardcoded spanish items when set to spanish", function () {
+        cy.visit("/?lang=es")
+        cy.get("#sba-footer").contains("Atención al cliente")
+        cy.get("#deskop-mini-nav").contains('Para los socios')
+    })
+
+    it("displays hardcoded english items when set to english", function () {
+        cy.visit("/")
+        cy.get("#sba-footer").contains("Customer Service")
+        cy.get("#deskop-mini-nav").contains('For Partners')
+    })
+
 })
