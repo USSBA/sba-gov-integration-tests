ca699d37bae31c3dc08484777091d2037b7a8305
Alex de los Reyes <areyes@fearless.tech>
2019-08-09 15:19:44 -0400

    TA-2926 Adding tests for district office page (#66)
    * TA-2926 Adding tests for district office page
    
    * TA-2926 Fixing a misspelling goof
    
    * TA-2926 Adding a 404 page test
    
    * TA-2926 Fixing the route target

---
+++ cypress/integration/tests/office-page.test.js
@@ -0,0 +1,22 @@
+describe("District Office Page", function () {
+    before(function(){
+        cy.request("GET", '/api/content/sbaOffices.json')
+        .then((result) => {
+            cy.wrap(result.body[0]).as("validOffice")
+        })
+    })
+
+    it("loads and pulls an office for content", function () {
+        cy.server()
+        cy.route("GET", `/api/content/${this.validOffice.id}.json`).as("OfficeRequest")
+        cy.visit(`/offices/district/${this.validOffice.id}`)
+        cy.wait("@OfficeRequest")
+        cy.contains(this.validOffice.title)
+    })
+
+    it("displays a 404 for a non existing office page", function() {
+        cy.visit("/offices/district/1", { failOnStatusCode: false }) // not a valid office
+        cy.get("[data-cy='error-page-title']").should("have.text", '404')
+        cy.get("[data-cy='error-page-message']").should("contain", 'local assistance page')
+    })
+})
