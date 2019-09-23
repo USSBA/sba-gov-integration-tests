87ee6f1630f713026177de84e91af195290f91b0
Amy DiPasquale <adipasquale@fearless.tech>
2019-09-23 09:40:33 -0400

    TA-2927 remove office param when no office id exists

--- cypress/integration/tests/blog-category-page.test.js
+++ cypress/integration/tests/blog-category-page.test.js
@@ -34,7 +34,7 @@ describe("Blog Category Page", function () {
         it(`${category.name} displays blogs for that category`, function(){
             cy.server()
             cy.fixture(`blogs/${category.fixture}`).as("BlogData")
-            cy.route("GET", `/api/content/search/blogs.json?category=${category.name}&office=&start=0&end=12`, `@BlogData`).as("BlogRequest")
+            cy.route("GET", `/api/content/search/blogs.json?category=${category.name}&start=0&end=12`, `@BlogData`).as("BlogRequest")
             cy.visit(category.url)
             cy.wait("@BlogRequest")
             cy.get("[data-testid='blog-category-title']")
@@ -77,7 +77,7 @@ describe("Blog Category Page", function () {
             const categoryNameRequestParam = 'Success Story'
 
             cy.route("GET", `/api/content/${mockOfficeId}.json`, "@OfficeData").as("OfficeRequest")
-            cy.route("GET", `/api/content/search/blogs.json?category=${categoryNameRequestParam}&office=${mockOfficeId}&start=0&end=12`, "@BlogData").as("BlogRequest")
+            cy.route("GET", `/api/content/search/blogs.json?category=${categoryNameRequestParam}&start=0&end=12&office=${mockOfficeId}`, "@BlogData").as("BlogRequest")
             cy.visit(`blogs/${categoryNameUrl}/${mockOfficeId}`)
             cy.wait("@OfficeRequest")
             cy.wait("@BlogRequest")
@@ -89,8 +89,8 @@ describe("Blog Category Page", function () {
 
     it("allows paginating through results", function(){
         cy.server()
-        cy.route("GET", `/api/content/search/blogs.json?category=Industry Word&office=&start=0&end=12`, this.IndustryWordDataPage1).as("BlogRequest1")
-        cy.route("GET", `/api/content/search/blogs.json?category=Industry Word&office=&start=12&end=24`,this.IndustryWordDataPage2).as("BlogRequest2")
+        cy.route("GET", `/api/content/search/blogs.json?category=Industry Word&start=0&end=12`, this.IndustryWordDataPage1).as("BlogRequest1")
+        cy.route("GET", `/api/content/search/blogs.json?category=Industry Word&start=12&end=24`,this.IndustryWordDataPage2).as("BlogRequest2")
 
         cy.visit("/blogs/industry-word")
         cy.wait("@BlogRequest1")
@@ -114,8 +114,8 @@ describe("Blog Category Page", function () {
 
     it("does not allow pagination backwards from the first page", function(){
         cy.server()
-        cy.route("GET", `/api/content/search/blogs.json?category=Industry Word&office=&start=0&end=12`, this.IndustryWordDataPage1).as("BlogRequest1")
-        cy.route("GET", `/api/content/search/blogs.json?category=Industry Word&office=&start=12&end=24`,this.IndustryWordDataPage2).as("BlogRequest2")
+        cy.route("GET", `/api/content/search/blogs.json?category=Industry Word&start=0&end=12`, this.IndustryWordDataPage1).as("BlogRequest1")
+        cy.route("GET", `/api/content/search/blogs.json?category=Industry Word&start=12&end=24`,this.IndustryWordDataPage2).as("BlogRequest2")
 
         cy.visit("/blogs/industry-word")
         cy.wait("@BlogRequest1")
@@ -130,8 +130,8 @@ describe("Blog Category Page", function () {
 
     it("does not allow pagination forwards from the last page", function(){
         cy.server()
-        cy.route("GET", `/api/content/search/blogs.json?category=Industry Word&office=&start=0&end=12`, this.IndustryWordDataPage1).as("BlogRequest1")
-        cy.route("GET", `/api/content/search/blogs.json?category=Industry Word&office=&start=12&end=24`,this.IndustryWordDataPage2).as("BlogRequest2")
+        cy.route("GET", `/api/content/search/blogs.json?category=Industry Word&start=0&end=12`, this.IndustryWordDataPage1).as("BlogRequest1")
+        cy.route("GET", `/api/content/search/blogs.json?category=Industry Word&start=12&end=24`,this.IndustryWordDataPage2).as("BlogRequest2")
 
         cy.visit("/blogs/industry-word")
         cy.wait("@BlogRequest1")
