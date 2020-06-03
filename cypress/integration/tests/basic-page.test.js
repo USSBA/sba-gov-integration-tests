3dd8aef095f925c403291417adb1e10f70ec4e97
Kevin Lee <ki.lee@sba.gov>
2020-06-03 11:36:43 -0400

    T1-1504 Add bizzie widget tests

--- cypress/integration/tests/basic-page.test.js
+++ cypress/integration/tests/basic-page.test.js
@@ -35,7 +35,7 @@ describe('Basic page', function() {
       cy.server()
       cy.fixture('contacts/microloans.json').as('MicroloanContacts')
       cy.route("GET", "/api/content/search/contacts.json?category=Microloan", "@MicroloanContacts" ).as("ContactRequest")
-      
+
       cy.visit('/partners/lenders/microloan-program/list-lenders')
       cy.get(".basicpage-titlesection > div[id^='lookup-']").as("ContactLookup")
       cy.get("@ContactLookup").get("[data-cy=lookup-select]").find('.Select-arrow-zone').click();
@@ -48,13 +48,56 @@ describe('Basic page', function() {
       cy.server()
       cy.fixture('contacts/microloans.json').as('MicroloanContacts')
       cy.route("GET", "/api/content/search/contacts.json?category=Microloan", "@MicroloanContacts" ).as("ContactRequest")
-      
+
       cy.visit('/partners/lenders/microloan-program/list-lenders')
       cy.get(".basicpage-titlesection > div[id^='lookup-']").as("ContactLookup")
       cy.get("@ContactLookup").get("[data-cy=lookup-select]").find('.Select-arrow-zone').click();
       cy.get("@ContactLookup").get("[data-cy=lookup-select]").contains("Alaska").click()
 
       cy.get("@ContactLookup").get('.contact-card').contains("Visit website").should("not.exist")
+    })
+  })
+
+  describe('bizzie bot', function(){
+    beforeEach(function() {
+      cy.visit('/about-sba/organization/contact-sba')
+    })
+
+    it('defaults to rendering in minimized state with correct styles and positions when visiting contact page', function() {
+      cy.get('[data-testid=bizzie-web-chat-container]').get('[data-testid=bizzie-web-chat-title-bar]').contains('Bizzie')
+      cy.get('[data-testid=bizzie-web-chat-container]').get('[data-testid=bizzie-web-chat-title-bar]').should('have.css', 'bottom', '7px')
+      cy.get('[data-testid=bizzie-web-chat-container]').get('[data-testid=bizzie-web-chat-title-bar]').should('have.css', 'left', '200px')
+      cy.get('[data-testid=bizzie-web-chat-container]').get('[data-testid=bizzie-web-chat-title-bar]').should('have.css', 'position', 'absolute')
+      cy.get('[data-testid=bizzie-web-chat-container]').get('[data-testid=bizzie-web-chat-title-bar]').should('have.css', 'width', '300px')
+      cy.get('[data-testid=bizzie-web-chat-container]').get('.bizzie-web-chat').should('have.css', 'visibility', 'hidden')
+    })
+
+    it('expands when clicking on the title bar with correct styles', function() {
+      cy.get('[data-testid=bizzie-web-chat-container]').should('have.css', 'bottom', '-7px')
+      cy.get('[data-testid=bizzie-web-chat-container]').should('have.css', 'height', '750px')
+      cy.get('[data-testid=bizzie-web-chat-container]').should('have.css', 'position', 'fixed')
+      cy.get('[data-testid=bizzie-web-chat-container]').should('have.css', 'right', '30px')
+      cy.get('[data-testid=bizzie-web-chat-container]').should('have.css', 'width', '500px')
+      cy.get('[data-testid=bizzie-web-chat-container]').get('[data-testid=bizzie-web-chat-title-bar]').click()
+      cy.get('[data-testid=bizzie-web-chat-container]').get('[data-testid=bizzie-web-chat-title-bar]').should('have.css', 'cursor', 'pointer')
+      cy.get('[data-testid=bizzie-web-chat-container]').get('[data-testid=bizzie-web-chat-title-bar]').should('have.css', 'height', '50px')
+      cy.get('[data-testid=bizzie-web-chat-container]').get('[data-testid=bizzie-web-chat-title-bar]').should('have.css', 'width', '500px')
+      cy.get('[data-testid=bizzie-web-chat-container]').get('.bizzie-web-chat').should('have.css', 'height', '700px')
+      cy.get('[data-testid=bizzie-web-chat-container]').get('.bizzie-web-chat').should('have.css', 'width', '500px')
+      cy.get('[data-testid=bizzie-web-chat-container]').get('.bizzie-web-chat').should('have.css', 'visibility', 'visible')
+    })
+
+    it('minimizes when clicking on the title bar', function() {
+      cy.get('[data-testid=bizzie-web-chat-container]').should('have.css', 'bottom', '-7px')
+      cy.get('[data-testid=bizzie-web-chat-container]').should('have.css', 'height', '750px')
+      cy.get('[data-testid=bizzie-web-chat-container]').should('have.css', 'position', 'fixed')
+      cy.get('[data-testid=bizzie-web-chat-container]').should('have.css', 'right', '30px')
+      cy.get('[data-testid=bizzie-web-chat-container]').should('have.css', 'width', '500px')
+      cy.get('[data-testid=bizzie-web-chat-container]').get('[data-testid=bizzie-web-chat-title-bar]').click().wait(2000).click()
+      cy.get('[data-testid=bizzie-web-chat-container]').get('[data-testid=bizzie-web-chat-title-bar]').should('have.css', 'cursor', 'pointer')
+      cy.get('[data-testid=bizzie-web-chat-container]').get('[data-testid=bizzie-web-chat-title-bar]').should('have.css', 'height', '50px')
+      cy.get('[data-testid=bizzie-web-chat-container]').get('[data-testid=bizzie-web-chat-title-bar]').should('have.css', 'width', '300px')
+      cy.get('[data-testid=bizzie-web-chat-container]').get('.bizzie-web-chat').should('have.css', 'visibility', 'hidden')
     })
   })
 })
