9b0be7f8385c6618ab8db92d71658e546d6393b1
mjackson21 <mjackson@fearless.tech>
2021-05-12 16:55:14 -0400

    TC-846 Fixed spacing

--- cypress/integration/tests/local-assistance-find.test.js
+++ cypress/integration/tests/local-assistance-find.test.js
@@ -96,9 +96,9 @@ describe("Local Assistance Find", function(){
         })
         
     it("Search bassed on Longitude and Latitude", function(){
-            cy.visit("local-assistance/find?pageNumber=1&mapCenter=39.919861127290275%2C-81.56856083125557&address=21075")
-            cy.get('#office-title-result-0')
-                .contains('District')
-                expect(cy.get("[data-testid='showing results text']").contains("Showing 1 - 5 of 50")).to.exist
+        cy.visit("local-assistance/find?pageNumber=1&mapCenter=39.919861127290275%2C-81.56856083125557&address=21075")
+        cy.get('#office-title-result-0')
+            .contains('District')
+            expect(cy.get("[data-testid='showing results text']").contains("Showing 1 - 5 of 50")).to.exist
         })
 })
